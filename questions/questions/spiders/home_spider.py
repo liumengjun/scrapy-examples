9c3e8c3e58388e897fb8f2d93f174a335ea0fb8f
Mengjun Liu <mengjun_liu@126.com>
2016-11-18 17:21:39 +0800

    refatcor visit home page

--- questions/questions/spiders/home_spider.py
+++ questions/questions/spiders/home_spider.py
@@ -5,19 +5,23 @@ from .. import settings
 
 class HomeSpider(scrapy.Spider):
     name = "home_page"
-
-    login_path = settings.LOGIN_PATH
-    home_path = settings.HOME_PATH
     cookie_store = {}
+
+    host = None
+    username = None
+    password = None
+    host_url = None
+    login_url = None
+    home_url = None
 
     def __init__(self, h=None, u=None, p=None, *args, **kwargs):
         super(HomeSpider, self).__init__(*args, **kwargs)
         self.host = h or settings.HOST
         self.username = u or settings.USERNAME
         self.password = p or settings.PASSWORD
-        self.host_url = 'http://%s' % self.host
-        self.login_url = self.host_url + self.login_path
-        self.home_url = self.host_url + self.home_path
+        self.host_url = '%s://%s' % (settings.PROTOCOL, self.host)
+        self.login_url = self.host_url + settings.LOGIN_PATH
+        self.home_url = self.host_url + settings.HOME_PATH
 
     def set_cookie_store(self, cookies):
         self.cookie_store = cookies
@@ -49,18 +53,22 @@ class HomeSpider(scrapy.Spider):
                 cookies[key] = val
         return cookies
 
+    def write_response_to_file(self, response, page_name=''):
+        page = response.url.split("/")[-2] or page_name
+        filename = 'questions-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+
     def parse(self, response: scrapy.http.Response):
         # print(response.url)
         # print(response.status)
         # print(response.headers)
         cookies = self.resolve_response_cookies(response)
         # print(cookies)
+        self.set_cookie_store(cookies)
 
-        page = response.url.split("/")[-2]
-        filename = 'questions-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.write_response_to_file(response)
 
         params = {'email': self.username, 'password': self.password, 'remeber': "0", 'next_url': ""}
         for key in settings.LOGIN_KEYS_IN_COOKIE:
@@ -76,8 +84,10 @@ class HomeSpider(scrapy.Spider):
         # print(cookies)
         self.set_cookie_store(cookies)
 
-        page = response.url.split("/")[-2]
-        filename = 'questions-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        self.write_response_to_file(response)
+
+        yield scrapy.Request(self.home_url, self.visit_pages,
+                             headers=settings.HEADERS, cookies=self.cookie_store)
+
+    def visit_pages(self, response):
+        self.write_response_to_file(response)
