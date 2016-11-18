d903e55a0ba0799aacf001128a2e8929fb275bd2
Mengjun Liu <mengjun_liu@126.com>
2016-11-18 16:59:10 +0800

    fix get home page

--- questions/questions/spiders/home_spider.py
+++ questions/questions/spiders/home_spider.py
@@ -8,6 +8,7 @@ class HomeSpider(scrapy.Spider):
 
     login_path = settings.LOGIN_PATH
     home_path = settings.HOME_PATH
+    cookie_store = {}
 
     def __init__(self, h=None, u=None, p=None, *args, **kwargs):
         super(HomeSpider, self).__init__(*args, **kwargs)
@@ -18,17 +19,63 @@ class HomeSpider(scrapy.Spider):
         self.login_url = self.host_url + self.login_path
         self.home_url = self.host_url + self.home_path
 
+    def set_cookie_store(self, cookies):
+        self.cookie_store = cookies
+
+    def update_cookie_store(self, cookies):
+        if self.cookie_store:
+            self.cookie_store.update(cookies)
+        else:
+            self.cookie_store = cookies
+
     def start_requests(self):
-        print(self.username)
-        print(self.password)
-        print(self.login_url)
+        # print(self.username)
+        # print(self.password)
+        # print(self.login_url)
         if not self.host or not self.username or not self.password:
             print("Parameters Error!")
             return None
-        yield scrapy.FormRequest(self.login_url, self.parse, formdata={
-            'email': self.username, 'password': self.password})
+        yield scrapy.Request(self.host_url, self.parse, headers=settings.HEADERS)
 
-    def parse(self, response):
+    def resolve_response_cookies(self, response):
+        cookies = {}
+        for s in response.headers.getlist('Set-Cookie'):
+            sc = s.decode()
+            pke = sc.find('=')
+            if pke != -1:
+                key = sc[:pke]
+                pve = sc.find(';', pke)
+                val = sc[pke + 1:pve]
+                cookies[key] = val
+        return cookies
+
+    def parse(self, response: scrapy.http.Response):
+        # print(response.url)
+        # print(response.status)
+        # print(response.headers)
+        cookies = self.resolve_response_cookies(response)
+        # print(cookies)
+
+        page = response.url.split("/")[-2]
+        filename = 'questions-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
+
+        params = {'email': self.username, 'password': self.password, 'remeber': "0", 'next_url': ""}
+        for key in settings.LOGIN_KEYS_IN_COOKIE:
+            params[key] = cookies.get(key, '')
+        yield scrapy.FormRequest(self.login_url, self.after_login, formdata=params,
+                                 headers=settings.HEADERS, cookies=cookies)
+
+    def after_login(self, response):
+        # print(response.url)
+        # print(response.status)
+        # print(response.headers)
+        cookies = self.resolve_response_cookies(response)
+        # print(cookies)
+        self.set_cookie_store(cookies)
+
         page = response.url.split("/")[-2]
         filename = 'questions-%s.html' % page
         with open(filename, 'wb') as f:
